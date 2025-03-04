73a7f00fa39b72c3decbd5ea792cddb286497371
KTV-2021-165\fashah.darullah <fashah.darulla@koltiva.com>
2025-03-04 13:14:07 +0700

    update pph

--- api/application/controllers/v1/admin/Payroll.php
+++ api/application/controllers/v1/admin/Payroll.php
@@ -617,24 +617,9 @@ class Payroll extends REST_Controller
 						<td>Rp ' . number_format($PayrollData["deduction_bpjs_kesehatan"], 2) . '</td>
 					</tr>
 					<tr>
-						<td>PPh 21 Gaji</td>
-						<td>:</td>
-						<td>Rp ' . number_format($PayrollData["deduction_pph_21"], 2) . '</td>
-					</tr>
-					<tr>
-						<td>PPh 21 Insentif</td>
+						<td>PPH Sesuai Tarif TER</td>
 						<td>:</td>
 						<td>Rp ' . number_format($PayrollData["deduction_pph_21_insentif"], 2) . '</td>
-					</tr>
-					<tr>
-						<td>PPh 21 Kompensasi</td>
-						<td>:</td>
-						<td>Rp ' . number_format($PayrollData["deduction_pph_21_kompensasi"], 2) . '</td>
-					</tr>
-					<tr>
-						<td>PPh 21 THR</td>
-						<td>:</td>
-						<td>Rp ' . number_format($PayrollData["deduction_pph_21_thr"], 2) . '</td>
 					</tr>
 					<tr>
 						<td>Kasbon</td>
